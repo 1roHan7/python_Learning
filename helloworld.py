25f227dd31ebf831e6d0baf54a02e083d051ffff
Rohan <137357237+1roHan7@users.noreply.github.com>
2024-11-15 21:44:10 +0000

    Added and explain First addition code in python

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,5 @@
-Print ("Hello world")
-Print ("Hello world world")
+## 1. write a python code to add two numbers - user input
+
+a = int(input("Enter the number"))                    ## by default if you input something it is treated as a string 
+b = int(input("Enter second number"))                 ## In order to add the user input value you need specify the data type of user input value
+print ("The Sum is " + str(a+b))                      ## Now again in order to concatenate the str value 'The Sum is' with the Int value you need to specify int as char.
