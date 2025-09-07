fe995d456621f5b4f8ff4aa47ef7f155fbfd023c
Rohan <137357237+1roHan7@users.noreply.github.com>
2025-09-07 16:51:32 +0000

    adding hollow and full pyramid pattern

--- star_patterns/Full_Pyramid_Patterns_in_Python.py
+++ star_patterns/Full_Pyramid_Patterns_in_Python.py
@@ -5,5 +5,14 @@
 # *********
 
 def full_pyramid_pattern(n):
-    
-    
+    l=0
+    for i in range (1,n+1):
+        
+        for j in range(1, n - i + 1):
+            print(" ", end="")
+        for k in range(1,i+l+1):
+            print("*",end="")
+            l=i
+        print()
+
+full_pyramid_pattern(5)
